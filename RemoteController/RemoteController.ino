155580724d142121f3ba3b78ff4aabcdcc4878e2
dalbyryan <dalbyryan3@gmail.com>
2020-02-14 18:56:47 -0700

    Fully implemented basic packetized communication

--- RemoteController/RemoteController.ino
+++ RemoteController/RemoteController.ino
@@ -11,6 +11,13 @@
 #define JOYSTICK_1_Y_PIN A1
 #define JOYSTICK_2_Y_PIN A2
 
+// Global state
+int mode = 1; // 1 is teleoperation mode
+int motor1Speed = -80;
+int motor2Speed = 200;
+int motor3Speed = 400;
+int motor4Speed = -400;
+int servoValue = 205; 
 
 SoftwareSerial xbeeSerial(XBEE_SERIAL_RX_PIN,XBEE_SERIAL_TX_PIN); // RX TX
 
@@ -28,8 +35,53 @@ void setup()
 
 void loop()
 {
-    xbeeSerial.write(250);
     delay(3000);
-    xbeeSerial.write(10);
-    delay(3000);
+    if(dataRequested())
+    {
+        // Determine state
+        // mode = mode; // IMPLEMENT READ MODE FROM MODE SWITCH
+        // servoValue = map(analogRead(JOYSTICK_2_Y_PIN), 0, 1024, 0, 255); // Will be written using PWM to servos, Arduino only has 0 to 255 range for PWM
+        motor1Speed = map(analogRead(JOYSTICK_1_Y_PIN), 0, 1024, -400, 400); // Will be written using the motor driver library directly to the motor with a speed between -400 and 400
+        motor2Speed = motor1Speed;
+        motor3Speed = motor1Speed;
+        motor4Speed = motor1Speed;
+
+        // Send state in packetized order
+
+        xbeeSerial.write(255); // Starting byte
+        // Send each state
+        xbeeSerialWriteInt(mode);
+        xbeeSerialWriteInt(servoValue);
+        xbeeSerialWriteInt(motor1Speed);
+        xbeeSerialWriteInt(motor2Speed);
+        xbeeSerialWriteInt(motor3Speed);
+        xbeeSerialWriteInt(motor4Speed);
+        Serial.println("PACKET SENT");
+        Serial.println();
+
+    }
+}
+
+// Method will return true if the Mega is asking for the current state
+bool dataRequested()
+{
+    // If Mega is asking for data then read the request and return true
+    if(xbeeSerial.available())
+    {
+        xbeeSerial.read(); 
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void xbeeSerialWriteInt(int value)
+{
+    byte high = highByte(value);
+    byte low = lowByte(value);
+    Serial.println(high << 8 | low);
+    xbeeSerial.write(high);
+    xbeeSerial.write(low);
 }
--- Sumotori/Sumotori.ino
+++ Sumotori/Sumotori.ino
@@ -1,7 +1,9 @@
 // ME EN 3220   Mechanical-Basho Arduino Mega 2560 Code     Ryan Dalby, Colby Smith, Aaron Fowlks Landon O'Camb
 
+
 // Libraries to be used
 #include <DualVNH5019MotorShieldMod3.h>
+
 
 // Pin defintions
 #define MOTOR_1_DIRECTION_A_PIN 2
@@ -29,7 +31,23 @@
 #define MOTOR_4_CURRENT_SENSE_PIN A4
 
 
+// Global objects
 DualVNH5019MotorShieldMod3 md(MOTOR_3_DIRECTION_A_PIN, MOTOR_3_DIRECTION_B_PIN, MOTOR_3_ENABLE_PIN, MOTOR_3_CURRENT_SENSE_PIN, MOTOR_3_SPEED_PIN, MOTOR_4_DIRECTION_A_PIN, MOTOR_4_DIRECTION_B_PIN, MOTOR_4_ENABLE_PIN, MOTOR_4_CURRENT_SENSE_PIN, MOTOR_4_SPEED_PIN);
+
+
+// Global state set to inital values
+int recievedData[6];
+int mode = 1; // 1 is teleoperation mode
+int motor1Speed = 0;
+int motor2Speed = 0;
+int motor3Speed = 0;
+int motor4Speed = 0;
+int servoValue = 0;
+
+long timeSinceLastPacket = millis(); // millis
+long packetWaitTime = 50000; // millis
+
+
 
 void setup()
 {
@@ -42,20 +60,77 @@ void setup()
     // Initialize the two DualVNH5019 motor shields
     md.init();
 
+    // Setup is complete
     Serial.println("Setup Complete");
 
+    // Ask for first state
+    Serial2.write(255);
+    // Serial.println("ASKING FOR INITIAL DATA");
 }
 
 void loop()
 {
-    while(Serial2.available())
+    if(Serial2.available())
     {
-        int speed = Serial2.read();
-        Serial.print("Setting all motor speeds to: ");
-        Serial.println(speed);
-        md.setM1Speed(speed);
-        md.setM2Speed(speed);
-        md.setM3Speed(speed);
-        md.setM4Speed(speed);
+        if(Serial2.read() == 255) // Extract starting byte
+        {
+            // Serial.println("DATA PACKET RECEIVED");
+            int integersRecieved = 0;
+            while (integersRecieved < 6)
+            {
+                if(Serial2.available() > 1)
+                {
+                    byte high = Serial2.read();
+                    byte low = Serial2.read();
+                    int reconstructedData = high << 8 | low;
+                    recievedData[integersRecieved] = reconstructedData;
+                    integersRecieved++;
+                    // Serial.println(reconstructedData);
+                }
+            }
+            // Serial.println();
+            // IMPLEMENT UPDATE MODE
+            mode = recievedData[0];
+
+            // Ask for more state
+            Serial2.write(255);
+            // Serial.println("ASKING FOR DATA");
+            // Set timeSinceLastPacket to current time
+            timeSinceLastPacket = millis();
+        }
     }
+
+    // IMPLEMENT CHECK MODE
+    if(mode == 1)
+    {
+        servoValue = recievedData[1];
+        motor1Speed = recievedData[2];
+        motor2Speed = recievedData[3];
+        motor3Speed = recievedData[4];
+        motor4Speed = recievedData[5];
+    }
+
+    // WRITE CURRENT STATE TO MOTORS AND SERVOS
+    // Serial.print("Setting motor 1 speed to ");
+    // Serial.println(motor1Speed);
+    md.setM1Speed(motor1Speed);
+    // Serial.print("Setting motor 2 speed to ");
+    // Serial.println(motor2Speed);
+    md.setM2Speed(motor2Speed);
+    // Serial.print("Setting motor 3 speed to ");
+    // Serial.println(motor3Speed);
+    md.setM3Speed(motor3Speed);
+    // Serial.print("Setting motor 4 speed to ");
+    // Serial.println(motor4Speed);
+    md.setM4Speed(motor4Speed);
+
+    // If no new state has been recieved for a while then ask again
+    // if (timeSinceLastPacket - millis() > packetWaitTime)
+    // {
+    //     Serial2.write(255);
+    //     Serial.println("ASKING FOR DATA BECAUSE OF WAIT TIME");
+    // }
+
 }
+
+
