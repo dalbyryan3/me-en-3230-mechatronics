54596fbd97c589187d827d69b5fd6ab9918d694b
dalbyryan <dalbyryan3@gmail.com>
2020-02-22 18:39:51 -0700

    Moved the calculation of motor speed to Sumotori. Using velocity as global state of sumotori rather than motor values. Cleaned up code.

--- RemoteController/RemoteController.ino
+++ RemoteController/RemoteController.ino
@@ -4,20 +4,22 @@
 #include <SoftwareSerial.h>
 
 // Pin defintions
-#define XBEE_SERIAL_RX_PIN 2
-#define XBEE_SERIAL_TX_PIN 3
+static const int XBEE_SERIAL_RX_PIN = 2;
+static const int XBEE_SERIAL_TX_PIN = 3;
+static const int JOYSTICK_1_X_PIN = A0;
+static const int JOYSTICK_1_Y_PIN = A1;
+static const int JOYSTICK_2_Y_PIN = A2;
 
-#define JOYSTICK_1_X_PIN A0
-#define JOYSTICK_1_Y_PIN A1
-#define JOYSTICK_2_Y_PIN A2
+// Global objects
+SoftwareSerial xbeeSerial(XBEE_SERIAL_RX_PIN,XBEE_SERIAL_TX_PIN); // RX TX
 
-// Global state
+// Global state set to initial values
 int16_t mode = 1; // 1 is teleoperation mode
 int16_t xVelocity = 0; // Will be a value between -10 and 10 (No actual units, just a relative speed value where 0 is not moving and 10 is maximum speed)
 int16_t yVelocity = 0; // Will be a value between -10 and 10
 int16_t servoIncrement = 0; // Will be avalue between -10 and 10 which indicates how many degrees to change the angle of the servos
 
-SoftwareSerial xbeeSerial(XBEE_SERIAL_RX_PIN,XBEE_SERIAL_TX_PIN); // RX TX
+
 
 void setup()
 {
@@ -35,44 +37,29 @@ void loop()
     // delay(3000);
     if(dataRequested())
     {
-        // Determine state
+        // Determine state remote controller 
         // mode = mode; // IMPLEMENT READ MODE FROM MODE SWITCH
         servoIncrement = map(analogRead(JOYSTICK_2_Y_PIN), 0, 1000, -10, 10); 
         xVelocity = map(analogRead(JOYSTICK_1_X_PIN), 0, 1024, -10, 10);
         yVelocity = map(analogRead(JOYSTICK_1_Y_PIN), 0, 1024, -10, 10); 
 
-        // motor1Speed = (motorSpeedVert + motorSpeedHorz);
-        // motor3Speed = (motorSpeedVert - motorSpeedHorz);
-        // if(motor1Speed > 400)
-        // {
-        //     motor1Speed = 400;
-        // }
-        // if(motor1Speed < -400)
-        // {
-        //     motor1Speed = -400;
-        // }
-        // if(motor3Speed > 400)
-        // {
-        //     motor3Speed = 400;
-        // }
-        // if(motor3Speed < -400)
-        // {
-        //     motor3Speed = -400;
-        // }
-        // motor2Speed = motor1Speed;
-        // motor4Speed = motor3Speed;
-
         // Send state in packetized order
+        // Will be sending 4 signed integers of data, mode, servoIncrement, xVelocity, and yVelocity
+        // [{start byte}, {mode byte 1}{mode byte 2}, {servo increment byte 1}{servo increment byte 2}, {xVelocity byte 1}{xVelocity byte 2}, {yVelocity byte 1}{yVelocity byte 2}]
         xbeeSerial.write(255); // Starting byte
         // Send each state
         xbeeSerialWriteInt(mode);
         xbeeSerialWriteInt(servoIncrement);
         xbeeSerialWriteInt(xVelocity);
         xbeeSerialWriteInt(yVelocity);
-        Serial.println("PACKET SENT");
-        Serial.println();
+        // Serial.println("PACKET SENT");
+        // Serial.println();
     }
 }
+
+
+
+// Methods which help abstract code above
 
 // Method will return true if the Mega is asking for the current state
 bool dataRequested()
--- Sumotori/Sumotori.ino
+++ Sumotori/Sumotori.ino
@@ -1,39 +1,36 @@
 // ME EN 3220   Mechanical-Basho Arduino Mega 2560 Code     Ryan Dalby, Colby Smith, Aaron Fowlks Landon O'Camb
 
-
 // Libraries to be used
 #include <DualVNH5019MotorShieldMod3.h>
 #include <PWMServo.h>
 
-
 // Pin defintions
-#define MOTOR_1_DIRECTION_A_PIN 2
-#define MOTOR_1_DIRECTION_B_PIN 4
-#define MOTOR_1_SPEED_PIN 9
-#define MOTOR_1_ENABLE_PIN 6
-#define MOTOR_1_CURRENT_SENSE_PIN A0
+static const int MOTOR_1_DIRECTION_A_PIN = 2;
+static const int MOTOR_1_DIRECTION_B_PIN = 4;
+static const int MOTOR_1_SPEED_PIN = 9;
+static const int MOTOR_1_ENABLE_PIN = 6;
+static const int MOTOR_1_CURRENT_SENSE_PIN = A0;
 
-#define MOTOR_2_DIRECTION_A_PIN 7
-#define MOTOR_2_DIRECTION_B_PIN 8
-#define MOTOR_2_SPEED_PIN 10
-#define MOTOR_2_ENABLE_PIN 12
-#define MOTOR_2_CURRENT_SENSE_PIN A1
+static const int MOTOR_2_DIRECTION_A_PIN = 7;
+static const int MOTOR_2_DIRECTION_B_PIN = 8;
+static const int MOTOR_2_SPEED_PIN = 10;
+static const int MOTOR_2_ENABLE_PIN = 12;
+static const int MOTOR_2_CURRENT_SENSE_PIN = A1;
 
-#define MOTOR_3_DIRECTION_A_PIN 24
-#define MOTOR_3_DIRECTION_B_PIN 26
-#define MOTOR_3_SPEED_PIN 45
-#define MOTOR_3_ENABLE_PIN 22
-#define MOTOR_3_CURRENT_SENSE_PIN A3
+static const int MOTOR_3_DIRECTION_A_PIN = 24;
+static const int MOTOR_3_DIRECTION_B_PIN = 26;
+static const int MOTOR_3_SPEED_PIN = 45;
+static const int MOTOR_3_ENABLE_PIN = 22;
+static const int MOTOR_3_CURRENT_SENSE_PIN = A3;
 
-#define MOTOR_4_DIRECTION_A_PIN 25
-#define MOTOR_4_DIRECTION_B_PIN 27
-#define MOTOR_4_SPEED_PIN 46
-#define MOTOR_4_ENABLE_PIN 23
-#define MOTOR_4_CURRENT_SENSE_PIN A4
+static const int MOTOR_4_DIRECTION_A_PIN = 25;
+static const int MOTOR_4_DIRECTION_B_PIN = 27;
+static const int MOTOR_4_SPEED_PIN = 46;
+static const int MOTOR_4_ENABLE_PIN = 23;
+static const int MOTOR_4_CURRENT_SENSE_PIN = A4;
 
-#define SERVO_1_PIN 11
-#define SERVO_2_PIN 13
-
+static const int SERVO_1_PIN = 11;
+static const int SERVO_2_PIN = 13;
 
 // Global objects
 DualVNH5019MotorShieldMod3 md(MOTOR_3_DIRECTION_A_PIN, MOTOR_3_DIRECTION_B_PIN, MOTOR_3_ENABLE_PIN, MOTOR_3_CURRENT_SENSE_PIN, MOTOR_3_SPEED_PIN, MOTOR_4_DIRECTION_A_PIN, MOTOR_4_DIRECTION_B_PIN, MOTOR_4_ENABLE_PIN, MOTOR_4_CURRENT_SENSE_PIN, MOTOR_4_SPEED_PIN);
@@ -41,21 +38,27 @@ PWMServo servo1;
 PWMServo servo2;
 
 // Global state set to inital values
-int recievedData[6];
+int recievedData[4];
 int mode = 1; // 1 is teleoperation mode
-int motor1Speed = 0;
-int motor2Speed = 0;
-int motor3Speed = 0;
-int motor4Speed = 0;
 
+// Motor state variables
+static const int MOTOR_UPPER_LIMIT = 400;
+static const int MOTOR_LOWER_LIMIT = -400;
+int xVelocity = 0;
+int yVelocity = 0;
+
+// Serial communication state variables
+long lastDataRequestTime = millis(); // milliseconds
+long packetWaitTime = 100;           // milliseconds
+
+// Servo state variables
+static const int SERVO_UPPER_LIMIT = 180; // degrees
+static const int SERVO_LOWER_LIMIT = 100; // degrees
 int servoPosition = 100;
 int servoIncrement = 0;
-
-long lastDataRequestTime = millis(); // milliseconds
-long packetWaitTime = 100; // milliseconds
-
 long lastServoMoveTime = 0; // milliseconds
-long servoWaitTime = 50; // milliseconds
+long servoWaitTime = 50;    // milliseconds
+
 
 
 void setup()
@@ -65,10 +68,11 @@ void setup()
 
     // Begin serial communication over the Xbee module, the Xbee will use pins 16 and 17 which correspond to Serial2 on the Mega
     Serial2.begin(9600);
-    
+
     // Initialize the two DualVNH5019 motor shields
     md.init();
 
+    // Initialize servo
     servo1.attach(SERVO_1_PIN);
     servo2.attach(SERVO_2_PIN);
     servo1.write(servoPosition);
@@ -86,23 +90,22 @@ void setup()
     // Serial.println("ASKING FOR INITIAL DATA");
 }
 
-
-
 void loop()
 {
-    if(Serial2.available() > 0)
+    // Recieve data if available
+    if (Serial2.available() > 0)
     {
-        if(Serial2.read() == 255) // Extract starting byte
+        if (Serial2.read() == 255) // Extract starting byte
         {
             // Serial.println("DATA PACKET RECEIVED");
             int integersRecieved = 0;
             while (integersRecieved < 6)
             {
-                if(Serial2.available() > 1)
+                if (Serial2.available() > 1)
                 {
                     byte high = Serial2.read();
                     byte low = Serial2.read();
-                    int reconstructedData = high << 8 | low;
+                    int16_t reconstructedData = high << 8 | low;
                     recievedData[integersRecieved] = reconstructedData;
                     integersRecieved++;
                     // Serial.println(reconstructedData);
@@ -120,76 +123,32 @@ void loop()
         }
 
         // Flush any extra data in the buffer
-        while(Serial2.available() > 0)
+        while (Serial2.available() > 0)
         {
             Serial2.read();
         }
     }
 
-
-
     // IMPLEMENT CHECK MODE
-    if(mode == 1)
+    // Determine mode and modify global sumotori state
+    if (mode == 1)
     {
         servoIncrement = recievedData[1];
-        motor1Speed = recievedData[2];
-        motor2Speed = recievedData[3];
-        motor3Speed = recievedData[4];
-        motor4Speed = recievedData[5];
+        xVelocity = recievedData[2];
+        yVelocity = recievedData[3];
     }
 
-
-
-    // WRITE CURRENT STATE TO MOTORS AND SERVOS
-    // Serial.print("Setting motor 1 speed to ");
-    // Serial.println(motor1Speed);
-    md.setM1Speed(motor1Speed);
-    // Serial.print("Setting motor 2 speed to ");
-    // Serial.println(motor2Speed);
-    md.setM2Speed(motor2Speed);
-    // Serial.print("Setting motor 3 speed to ");
-    // Serial.println(motor3Speed);
-    md.setM3Speed(motor3Speed);
-    // Serial.print("Setting motor 4 speed to ");
-    // Serial.println(motor4Speed);
-    md.setM4Speed(motor4Speed);
-
-    if((millis() - lastServoMoveTime) > servoWaitTime)
+    // Write current motor state
+    writeVelocityToMotors();
+    // Write current servo state
+    if ((millis() - lastServoMoveTime) > servoWaitTime)
     {
-        servoPosition += servoIncrement;
-        if(servoPosition > 180)
-        {
-            servoPosition = 180;
-        }
-        if(servoPosition < 100)
-        {
-            servoPosition = 100;
-        }
-        // if(servoPosition > 180)
-        // {
-        //     servoPosition = 180;
-        // }
-        // if(servoPosition < 0)
-        // {
-        //     servoPosition = 0;
-        // }
-        servo1.attach(SERVO_1_PIN);
-        servo2.attach(SERVO_2_PIN);
-        servo1.write(servoPosition);
-        servo2.write(map(servoPosition, 100, 180, 180, 100));
-        delay(100);
-        servo1.detach();
-        servo2.detach();
-        Serial.println(servo1.read());
-        Serial.println(servo2.read());
-        Serial.println();
+        writeServoIncrementToServos();
+        // Serial.println(servo1.read());
+        // Serial.println(servo2.read());
+        // Serial.println();
         lastServoMoveTime = millis();
-
     }
-
-
-
-
 
     // If no new state has been recieved for a while then ask again
     if ((millis() - lastDataRequestTime) > packetWaitTime)
@@ -198,7 +157,76 @@ void loop()
         lastDataRequestTime = millis();
         // Serial.println("ASKING FOR DATA BECAUSE OF WAIT TIME");
     }
-
 }
 
 
+
+// Methods which help abstract code above
+
+// Used translate and write the velocity global state into motor commands
+// Will limit the written motor commands to possible values
+void writeVelocityToMotors()
+{
+    // Will use differential steering between right and left motors to turn sumotori
+
+    int motorSpeedVert = map(yVelocity, -10, 10, MOTOR_LOWER_LIMIT, MOTOR_UPPER_LIMIT);
+    int motorSpeedHorz = map(xVelocity, -10, 10, MOTOR_LOWER_LIMIT, MOTOR_UPPER_LIMIT);
+    int rightMotorSpeed = (motorSpeedVert + motorSpeedHorz); // Motors 1 and 2
+    int leftMotorSpeed = (motorSpeedVert - motorSpeedHorz);  // Motors 3 and 4
+
+    // Apply upper and lower limits to motor values
+    if(rightMotorSpeed > MOTOR_UPPER_LIMIT)
+    {
+        rightMotorSpeed = MOTOR_UPPER_LIMIT;
+    }
+    if(rightMotorSpeed < MOTOR_LOWER_LIMIT)
+    {
+        rightMotorSpeed = MOTOR_LOWER_LIMIT;
+    }
+    if(leftMotorSpeed > MOTOR_UPPER_LIMIT)
+    {
+        leftMotorSpeed = MOTOR_UPPER_LIMIT;
+    }
+    if(leftMotorSpeed < MOTOR_LOWER_LIMIT)
+    {
+        leftMotorSpeed = MOTOR_LOWER_LIMIT;
+    }
+
+    // Write motor speeds
+    md.setM1Speed(rightMotorSpeed);
+    // Serial.print("Setting motor 2 speed to ");
+    // Serial.println(motor2Speed);
+    md.setM2Speed(rightMotorSpeed);
+    // Serial.print("Setting motor 3 speed to ");
+    // Serial.println(motor3Speed);
+    md.setM3Speed(leftMotorSpeed);
+    // Serial.print("Setting motor 4 speed to ");
+    // Serial.println(motor4Speed);
+    md.setM4Speed(leftMotorSpeed);
+}
+
+// Used to translate and write the servo global state into servo write commands
+// Will limit the written servo commands to possible values
+void writeServoIncrementToServos()
+{
+    servoPosition += servoIncrement;
+
+    // Apply upper and lower limits to servo position
+    if (servoPosition > SERVO_UPPER_LIMIT)
+    {
+        servoPosition = 180;
+    }
+    if (servoPosition < SERVO_LOWER_LIMIT)
+    {
+        servoPosition = 100;
+    }
+
+    // Write servo values
+    servo1.attach(SERVO_1_PIN);
+    servo2.attach(SERVO_2_PIN);
+    servo1.write(servoPosition);
+    servo2.write(map(servoPosition, 100, 180, 180, 100));
+    delay(100);
+    servo1.detach();
+    servo2.detach();
+}
